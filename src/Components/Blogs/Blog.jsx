517ce84045ad077f575f5515d5d3f1f32335d218
VarshilJPatel <mail.varshil@gmail.com>
2025-04-08 19:18:10 +0530

    feat(blog): added navigation

--- src/Components/Blogs/Blog.jsx
+++ src/Components/Blogs/Blog.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
-const Blog = () => {
+const Blog = ({ title, description, date, slug }) => {
+  const navigate = useNavigate();
+
+  function handleOpenBlog() {
+    navigate(`blog/${slug}`);
+  }
+
   return (
-    <>
-    <div className='h-[75vh] bg-gray-300'></div>
-    </>
-  )
-}
+    <div
+      className="relative h-[70vh] w-full bg-[#E6D7D7] group"
+      id="image_container"
+    >
+      <div
+        onClick={handleOpenBlog}
+        id="content_container"
+        className="absolute w-full bottom-0 bg-red-800 group-hover:p-4 flex flex-col gap-4 cursor-pointer h-0 group-hover:h-full overflow-hidden transition-all duration-250 ease-in-out"
+      >
+        <p className="opacity-0 group-hover:opacity-100 transition-opacity duration-500">
+          {date}
+        </p>
+        <h1 className="text-[1.5rem] opacity-0 group-hover:opacity-100 transition-opacity duration-500">
+          {title}
+        </h1>
+        <p className="opacity-0 group-hover:opacity-100 transition-opacity duration-500">
+          {description}
+        </p>
+        <span className="cursor-pointer mt-4 hover:underline underline-offset-4">
+          Read more
+        </span>
+      </div>
+    </div>
+  );
+};
 
-export default Blog
+export default Blog;
--- src/Components/Blogs/index.jsx
+++ src/Components/Blogs/index.jsx
@@ -1,16 +1,25 @@
 import React from "react";
 import Blog from "./Blog.jsx";
 
-const postMap = {};
+const blogsMap = new Array(3).fill({
+  title: "A few words about us",
+  description: "Lorem ipsum dolor sit amet  suscipit sapiente esse temporibus quae, nihil voluptates, harum illum laboriosam?",
+  date: "08 - 12 - 2025",
+  slug: 1
+});
 
 const Blogs = () => {
   return (
-    <div className="px-8">
+    <div className="px-8 flex flex-col w-full">
       <h1 className="text-[6rem] font-sans font-normal">Posts</h1>
-      <div className="grid gap-8 grid-cols-3 col-span-3">
-        <Blog />
-        <Blog />
-        <Blog />
+      <div className="grid gap-8 max-xl:grid-cols-1 xl:grid-cols-3">
+        {blogsMap.map((blog, index) => {
+          return (
+            <div key={index}>
+              <Blog title={blog.title} slug={blog.slug} description={blog.description} date={blog.date} />
+            </div>
+          );
+        })}
       </div>
     </div>
   );
