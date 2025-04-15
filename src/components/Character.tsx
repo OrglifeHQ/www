e9d8dc0ecab4ab483eb109e7311653b6cb542bcb
varshiljpatel <mail.varshil@gmail.com>
2025-04-15 17:04:46 +0530

    feat: adding model spine

---
+++ src/components/Character.tsx
@@ -0,0 +1,11 @@
+import Spline from '@splinetool/react-spline/next';
+
+export default function Character() {
+  return (
+    <div className='absolute top-0 bottom-0 left-0 right-0'>
+      <Spline
+        scene="https://prod.spline.design/pnS7Tyvt8Udiigdj/scene.splinecode" 
+      />
+    </div>
+  );
+}
